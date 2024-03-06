6c13f38f5749412be6935fb7180c4070568a1683
eoeo <naiga6@gmail.com>
2024-03-06 16:45:24 +0900

    secretKey 생성

--- src/test/java/com/ll/jwt/JwtApplicationTests.java
+++ src/test/java/com/ll/jwt/JwtApplicationTests.java
@@ -1,13 +1,22 @@
 package com.ll.jwt;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 class JwtApplicationTests {
 
+    @Value("${custom/jwt/secretKey}")
+    private String secretKey;
+
     @Test
-    void contextLoads() {
+    @DisplayName("secretKey 필수")
+    void t1() {
+        assertThat(secretKey).isNotNull();
     }
 
 }
